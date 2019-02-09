e79f5ef029784d3889c4f7a467bec3e071588f68
WonTaek <time_to_bye@naver.com>
2019-02-10 03:54:58 +0900

    5063.py solution

---
+++ 5063.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    r, e, c = map(int, input().split())
+
+    if r < (e-c):
+        print("advertise")
+    elif r == (e-c):
+        print("does not matter")
+    else:
+        print("do not advertise")
