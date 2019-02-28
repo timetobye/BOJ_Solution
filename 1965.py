93d247db91e7315c2c09c655866a09e2529dde19
WonTaek <time_to_bye@naver.com>
2019-03-01 01:40:50 +0900

    1965.py solution LIS

---
+++ 1965.py
@@ -0,0 +1,40 @@
+"""
+n^2 풀이 방식
+전형적인 LIS 문제
+"""
+
+
+# n = int(input())
+# arr = [int(x) for x in input().split()]
+# arr.insert(0, 0)
+#
+# dp = [1 for x in range(n + 1)]
+#
+# for i in range(1, n + 1):
+#     for j in range(1, i):
+#         if arr[j] < arr[i]:
+#             dp[i] = max(dp[i], dp[j] + 1)
+#
+# print(max(dp))
+
+
+"""
+nlog * n 으로 풀기
+"""
+
+from bisect import bisect_left
+
+
+n = int(input())
+arr = [int(x) for x in input().split()]
+arr.insert(0, 0)
+
+dp = []
+
+for i in range(1, n + 1):
+    if (not dp) or (dp[-1] < arr[i]):
+        dp.append(arr[i])
+    else:
+        dp[bisect_left(dp, arr[i])] = arr[i]
+
+print(len(dp))
