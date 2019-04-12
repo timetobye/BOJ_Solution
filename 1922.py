328d7522663b4d2e1c2b77a5a0cb237ee30a67b7
WonTaek <time_to_bye@naver.com>
2019-04-12 23:31:45 +0900

    MST 1922.py solution

---
+++ 1922.py
@@ -0,0 +1,86 @@
+"""
+method 1
+"""
+
+from sys import stdin
+
+n = int(input())
+k = int(input())
+
+edges = []
+for i in range(k):
+    edge = [int(x) for x in stdin.readline().strip().split()]
+    edges.append(edge)
+
+# 가중치를 작은 순서대로 정렬한다.
+sorted_edges = sorted(edges, key=lambda edge : edge[2])
+
+
+# list element is None
+parent = [None for i in range(n + 1)]
+
+
+# set the find_set
+def find_set(x):
+    if parent[x] is None:
+        return x
+
+    return find_set(parent[x])
+
+
+def union(x, y):
+    parent_x = find_set(x)
+    parent_y = find_set(y)
+    parent[parent_x] = parent_y
+
+
+sum_weight = 0
+for u, v, w in sorted_edges:
+    if find_set(u) != find_set(v):
+        union(u, v)
+        sum_weight += w
+
+print(sum_weight)
+
+"""
+method 2
+"""
+
+from sys import stdin
+
+n = int(input())
+k = int(input())
+
+edges = []
+for i in range(k):
+    edge = [int(x) for x in stdin.readline().strip().split()]
+    edges.append(edge)
+
+# 가중치를 작은 순서대로 정렬한다.
+sorted_edges = sorted(edges, key=lambda edge : edge[2])
+
+# list element is number
+parent = list(range(1 + n))
+
+
+# set the find set
+def find_set(x):
+    if parent[x] != x:
+        return find_set(parent[x])
+
+    return x
+
+
+def union(x, y):
+    parent_x = find_set(x)
+    parent_y = find_set(y)
+    parent[parent_x] = parent_y
+
+
+sum_weight = 0
+for u, v, w in sorted_edges:
+    if find_set(u) != find_set(v):
+        union(u, v)
+        sum_weight += w
+
+print(sum_weight)
