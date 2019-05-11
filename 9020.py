0844353979fb4b22eeb628511e343a6f081f2bcf
WonTaek <time_to_bye@naver.com>
2019-05-12 01:44:42 +0900

    add solution

--- 9020.py
+++ 9020.py
@@ -1,3 +1,11 @@
+"""
+1. 소수 배열을 구한다. 0인 경우는 소수가 아니 것으로 설정
+2. n = prime_lst[x] + prime_lst[n-x] 로 설정하면 모든 것을 순회하지 않고 조회 할 수 있다.
+3. 이렇게 하면 시간 초과를 피할 수 있다.
+4. prime_gap 는 임의로 크게 설정함
+"""
+
+
 from math import sqrt
 
 
@@ -23,9 +31,7 @@ for i in range(2, 10000 + 1):
 
 for test in range(test_case):
     n = int(input())
-
     prime_gap = 100000**2
-    res_prime_one, res_prime_two = 0, 0
 
     for i, value in enumerate(prime_lst):
         if (value > 0) and (prime_lst[n-i] > 0):
