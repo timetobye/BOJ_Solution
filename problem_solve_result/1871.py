d0e80ca053e770b59f2175657845b362c650560f
WonTaek <time_to_bye@naver.com>
2021-07-24 02:38:35 +0900

    update solution

--- problem_solve_result/1871.py
+++ problem_solve_result/1871.py
@@ -1,3 +1,36 @@
+from sys import stdin
+
+
+def main():
+    n = int(stdin.readline())
+    
+    for _ in range(n):
+        string_and_number = stdin.readline().split('-')
+        string = string_and_number[0]
+        string_sum = 0
+        
+        for i in range(len(string)):
+            string_sum += (ord(string[i])-65)*26**(2-i)
+        
+        number = int(string_and_number[1])
+        result = abs(string_sum - number)
+
+        if result <= 100:
+            print("nice")
+        else:
+            print("not nice")
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+"""
+재채점을 했을 때 틀렸다고 나와서 코드를 살펴보니 오류가 있었다.
+62 -> 65로 통일을 하고, 가독성이 떨어지는 부분을 개선하였다.
+
 n = int(input())
 
 for i in range(n):
@@ -8,4 +41,6 @@ for i in range(n):
     if result <= 100:
         print("nice")
     else:
-        print("not nice")  
+        print("not nice")
+
+"""
